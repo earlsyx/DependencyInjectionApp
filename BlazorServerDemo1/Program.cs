9f343971676c8e54a45e4e3768a48b3ad32b3869
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-09-14 20:22:08 +0800

    singlton - 1 instance 4ever. same instance. - single instance. refer back to same instance.
    - no memory turn.
    alwys get same one.
    
    - sharing data afcross diff web user.
    - danger. , allwos optons.
    - good idea, if loading app config,
    to acces info, configuring web app.
    laoding it once
    pertime uyou run.
    reccle actual web sever, not happen quitelty.
    
    live for the life ofyour weg app,
    nopt release memory.
    
    not realese, still in meomory.

--- BlazorServerDemo1/Program.cs
+++ BlazorServerDemo1/Program.cs
@@ -14,7 +14,7 @@ namespace BlazorServerDemo1
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
-            builder.Services.AddTransient<Demo>();
+            builder.Services.AddSingleton<Demo>();
             builder.Services.AddTransient<ProcessDemo>();
 
             var app = builder.Build();
