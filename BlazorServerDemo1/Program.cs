404a00ddad351988bf874cb6a06f93ad1bac40b9
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-09-14 20:25:01 +0800

    -perscope it reate 1 instance. lie isngltnion but per use.
    - scope everyitm erequeist in server
    - middlgornd singlton onl per call.

--- BlazorServerDemo1/Program.cs
+++ BlazorServerDemo1/Program.cs
@@ -14,7 +14,7 @@ namespace BlazorServerDemo1
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
-            builder.Services.AddSingleton<Demo>();
+            builder.Services.AddScoped<Demo>();
             builder.Services.AddTransient<ProcessDemo>();
 
             var app = builder.Build();
