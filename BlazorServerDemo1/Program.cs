6c4dd123f0ec62efd19411143f15cf8d7c7b20c3
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-09-14 20:00:23 +0800

    register - transient - it will create new instane everytime you instantiate it. - scope - singlton

--- BlazorServerDemo1/Program.cs
+++ BlazorServerDemo1/Program.cs
@@ -14,6 +14,7 @@ namespace BlazorServerDemo1
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddTransient<Demo>();
 
             var app = builder.Build();
 
