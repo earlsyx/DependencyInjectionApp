f351bedfca741bce205135be8b9c46704d9d7de8
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-09-16 05:43:31 +0800

    dipose servicses

--- BlazorServerDemo1/Data/Demo.cs
+++ BlazorServerDemo1/Data/Demo.cs
@@ -1,13 +1,20 @@
 ﻿namespace BlazorServerDemo1.Data
 {
-    public class Demo : IDemo, ILocalDemo
+    public class Demo : IDemo, ILocalDemo, IDisposable
     {
+        private readonly ILogger<Demo> _log;
+
         public DateTime StartUpTime { get; init; }
 
-        public Demo()
+        public Demo(ILogger<Demo> log)
         {
             StartUpTime = DateTime.Now;
+            _log = log;
         }
 
+        public void Dispose()
+        {
+            _log.LogError($"Demo {StartUpTime.ToString("hh:mm:ss ffffff")}was disposed of");
+        }
     }
 }
